7f7a257b9500d3cc867113402b17be368efa0a3d
Shawn Singh <SSingh@aftersoftna.com>
2019-05-10 13:16:15 -0400

    Removed ftp creds

--- amz-ftp.js
+++ amz-ftp.js
@@ -1,21 +1,18 @@
 var Client = require('ftp');
 var fs = require('fs');
+var config = require('./config');
 
 var c = new Client();
 
-var config = {
-	host: "amazon.openwebs.com",
-	port: 22,
-	user: "mamuser",
-	password: "techn0l0gies!!"
-}
+
 
 c.on('ready', function(){
-	c.list('Incoming_SDC', function(err, list) {
+	c.list(process.argv[2], function(err, list) {
 		if (err) throw err;
 		var count = 0;
 		for(var i=0; i < list.length; i++){
-			if (list[i].type == '-' && (list[i].name.search('_ACES') != -1 || list[i].name.search('_PIES') != -1)) {
+			if (list[i].type == '-' && (list[i].name.search('ACES') != -1 || list[i].name.search('PIES') != -1)) {
+				//console.dir(list[i].name + ": " + list[i].name.slice(9, 13) + ": " + list[i].name.slice(4,8));
 				console.dir(list[i].name);
 				count++;
 			}
--- comparexml.js
+++ comparexml.js
@@ -1,34 +1,66 @@
 fs = require('fs');
-var parser = require('xml2json');
+//var parser = require('xml2json');
 var xmldoc = require('xmldoc');
 var xmlcompare = require('node-xml-compare');
 
+xml1 = "<sample><a>1</a><a>2</a><a>4</a><b>4</b></sample>";
+xml2 = "<sample><a>2</a><a>1</a><a>3</a><c>3</c></sample>";
 
-function initialize(){
-	xml-path-1 = "C:\Users\SSingh\Documents\aces-pies-verify\testfiles\BPI_Raybestos_Friction_PIES_2019-01-18_FULL.xml";
-
-	xml-path-2 = "C:\Users\SSingh\Documents\aces-pies-verify\testfiles\BPI_Raybestos_Friction_PIES_2019-01-25_FULL.xml";
-
+function initialize1(){
+	xmlpath1 = "C:\\Users\\SSingh\\Documents\\aces-pies-verify\\testfiles\\BPI_Raybestos_Friction_PIES_2019-01-18_FULL.xml";
 	return new Promise(function(resolve, reject) {
-		fs.readFile(xml-path-1, 'utf8', (err, data) => {
+		fs.readFile(xmlpath1, 'utf8', (err, data) => {
 			if (err) {
 				reject(err);
 			} else {
 				var xml = new xmldoc.XmlDocument(data);
-				console.log("Brand Code: " + xml.valueWithPath("Items.Item.BrandAAIAID"));
+				//resolve("Brand Code: " + xml.valueWithPath("Items.Item.BrandAAIAID"));
+				//resolve("Item Effective Date: " + xml.valueWithPath("Header.BlanketEffectiveDate"));
+				//var effectdate1 = xml.valueWithPath("Header.BlanketEffectiveDate");
+				resolve(data.toString());
+			}
+		});
+	});	
+}
+
+function initialize2(){
+	xmlpath2 = "C:\\Users\\SSingh\\Documents\\aces-pies-verify\\testfiles\\BPI_Raybestos_Friction_PIES_2019-01-25_FULL.xml";
+	return new Promise(function(resolve, reject) {
+		fs.readFile(xmlpath2, 'utf8', (err, data) => {
+			if (err) {
+				reject(err);
+			} else {
+				var xml = new xmldoc.XmlDocument(data);
+				//resolve("Brand Code: " + xml.valueWithPath("Items.Item.BrandAAIAID"));
+				//resolve("Item Effective Date: " + xml.valueWithPath("Header.BlanketEffectiveDate"));
+				//var effectdate2 = xml.valueWithPath("Header.BlanketEffectiveDate");
+				resolve(data.toString());
 			}
 		});
 	});	
 }
 
 function main(){
-	var initializePromise = initialize();
-	initializePromise.then( (result) => {
-		console.log("initialized...");
-		console.log(result);
+	var initializePromise1 = initialize1();
+	var initializePromise2 = initialize2();
+	Promise.all([initializePromise1, initializePromise2]).then((val) => {
+		xmlcompare(val[0], val[1], (result) => {
+			console.log(result['-']);
+		});
+		console.log(val[0]);
 	});
+	// xmlcompare(xml1,xml2, (result) => {
+	// 	console.log(result['-']);
+	// });
+
+
 }
 
+// xmlcompare(xml1, xml2, (result) => {
+// 	console.log(result['-']);
+// 	console.log('---------');
+// 	console.log(result['+']);
+// });
 main();
 
 
